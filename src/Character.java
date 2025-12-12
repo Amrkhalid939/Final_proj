355f217488b252945f2bba0b91cdb3491a7b0bf0
Mohmed Ahmed <bahour2005@gmail.com>
2025-12-12 15:57:38 +0200

    ay habl

--- src/Character.java
+++ src/Character.java
@@ -13,6 +13,8 @@ public class Character {
     private double armor;
     private int level;
 
+    //kwjhfkjwehfkjadfhjkh
+
     public Character(String name, String race, String type, Weapon weapon, double range,  double health, double damage, double speed, double armor, int level) {
         this.name = name;
         this.race = race;
